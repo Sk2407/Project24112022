9c42b02254b00477e623c71f192a0352d717d22f
sk2407 <sachinkumar24071999@gmail.com>
2022-11-24 21:34:43 +0530

    new project added

---
+++ src/main/java/com/example/project24112022/Project24112022Application.java
@@ -0,0 +1,14 @@
+package com.example.project24112022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project24112022Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Project24112022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project24112022/Project24112022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project24112022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project24112022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
